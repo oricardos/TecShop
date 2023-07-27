d0c7054bef647fe0939f78901ccd4ae4ce3207f1
oricardos <ricardo.rs95@gmail.com>
2023-07-26 23:21:52 -0300

    feat(item): ocultando quantidade

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import { AmountInput } from "../AmountInput";
 import { Button } from "../Button";
@@ -7,6 +7,7 @@ import { Button } from "../Button";
 export const Item = ({ name, img, price, description }) => {
   const [amount, setAmount] = useState(1);
   const [finalPrice, setFinalPrice] = useState(0);
+  const [expand, setExpand] = useState(false);
 
   const updateAmount = (newAmount) => {
     setAmount(newAmount);
@@ -17,6 +18,11 @@ export const Item = ({ name, img, price, description }) => {
     setFinalPrice(price * amount);
   };
 
+  const expandInverted = () => {
+    setExpand(!expand);
+    updateAmount(1);
+  };
+
   useEffect(() => {
     updateAmount(amount);
   }, []);
@@ -25,35 +31,39 @@ export const Item = ({ name, img, price, description }) => {
     <View style={styles.wrapperItem}>
       <Image source={img} style={styles.itemImg} />
       <View style={styles.itemInfo}>
-        <Text style={styles.itemName}>{name}</Text>
-        <Text style={styles.itemDescription}>{description}</Text>
-        <Text style={styles.itemPrice}>
-          {Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(price)}
-        </Text>
+        <TouchableOpacity onPress={expandInverted}>
+          <Text style={styles.itemName}>{name}</Text>
+          <Text style={styles.itemDescription}>{description}</Text>
+          <Text style={styles.itemPrice}>
+            {Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(price)}
+          </Text>
+        </TouchableOpacity>
 
-        <View style={styles.addItem}>
-          <View style={styles.item}>
-            <View style={styles.amountContainer}>
-              <Text style={styles.amount}>Quantidade: </Text>
-              <AmountInput setAmount={updateAmount} value={amount} />
+        {expand ? (
+          <View style={styles.addItem}>
+            <View style={styles.item}>
+              <View style={styles.amountContainer}>
+                <Text style={styles.amount}>Quantidade: </Text>
+                <AmountInput setAmount={updateAmount} value={amount} />
+              </View>
+
+              <View style={styles.priceContainer}>
+                <Text style={styles.price}>Total:</Text>
+                <Text style={styles.finalPrice}>
+                  {Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(finalPrice)}
+                </Text>
+              </View>
             </View>
 
-            <View style={styles.priceContainer}>
-              <Text style={styles.price}>Total:</Text>
-              <Text style={styles.finalPrice}>
-                {Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                }).format(finalPrice)}
-              </Text>
-            </View>
+            <Button text={"Adicionar"} />
           </View>
-
-          <Button text={"Adicionar"} />
-        </View>
+        ) : null}
       </View>
 
       <View style={styles.divider} />
